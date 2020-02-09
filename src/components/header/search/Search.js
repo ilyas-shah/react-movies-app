7c6ca5a8c123ab0268eb1311c6b83c439fc240f4
kkolekar96 <komal.kolekar@cumminscollege.in>
2020-02-09 11:51:35 +0530

    Added header styling.

--- src/components/header/search/Search.js
+++ src/components/header/search/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import getSuggestions from "./samplelist";
+import "../../../styles/Search.scss";
 
 const Search = () => {
   const [query, setQuery] = useState("");
