cf339b883865d800e4de7a68cc8791241b0c1cbc
ilyas-srijan <ilyas-srijan@SomeOne.local>
2020-02-13 18:03:47 +0530

    add main content div

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Header from "./header/Header";
+import Carousel from "./carousel/Carousel";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <div className="main-content" style={{ position: "relative" }}>
+          <Carousel />
+        </div>
         <Switch>
           <Route path="/" exact={true} />
         </Switch>
