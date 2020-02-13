e01a7937723f1d5d0afdc20eb53e62cf721404a3
ilyas-srijan <ilyas-srijan@SomeOne.local>
2020-02-13 18:04:19 +0530

    add logo

--- src/components/header/logo/Logo.js
+++ src/components/header/logo/Logo.js
@@ -4,9 +4,8 @@ import { NavLink } from "react-router-dom";
 const Logo = () => {
   return (
     <>
-      <NavLink to="/" className="logo">
-        {/* <img src="./logo192.png" alt="logo" /> */}
-        <strong>MOVIE FLIX</strong>
+      <NavLink to="/home">
+        <div className="logo"></div>
       </NavLink>
     </>
   );
